25b890e83bfdce21c4f37b7e47a922632007c61f
Mayra Barros Belizón <94868089+MayraBarros@users.noreply.github.com>
2022-01-22 20:00:35 +0100

    Add files via upload

--- js-animation.js
+++ js-animation.js
@@ -77,5 +77,53 @@ function vueltaUnoRp() {
 }
 
 
+// Galeria
+const btnCierra = document.querySelector('#btn-cierra');
+const btnRetrocede = document.querySelector('#btn-retrocede');
+const btnAdelanta = document.querySelector('#btn-adelanta');
+const imagenes = document.querySelectorAll('#galeria img');
+const lightbox = document.querySelector('#contenedor-principal-galeria');
+const imagenActiva = document.querySelector('#img-activa');
+let indiceImagen = 0;
+
+//Abrir lightbox//
+const abreLightbox = (event) =>{
+    imagenActiva.src = event.target.src;
+    lightbox.style.display = 'flex';
+    indiceImagen = Array.from(imagenes).indexOf(event.target);
+};
+
+imagenes.forEach((imagenes) => {
+    imagenes.addEventListener('click', abreLightbox);
+});
 
 
+//cerrar lightbox//
+
+btnCierra.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+})
+
+
+//adelantar imagen
+
+const adelantarImagen = () => {
+    if(indiceImagen === imagenes.length - 1) {
+        indiceImagen = -1;
+    }
+    imagenActiva.src = imagenes[indiceImagen + 1].src;
+    indiceImagen++;
+};
+
+btnAdelanta.addEventListener('click', adelantarImagen);
+
+// retroceder imagen
+const retrocedeImagen = () => {
+    if(indiceImagen === 0){
+        indiceImagen = imagenes.length;
+    }
+    imagenActiva.src = imagenes[indiceImagen - 1].src;
+    indiceImagen--;
+}
+
+btnRetrocede.addEventListener('click', retrocedeImagen);
